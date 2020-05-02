6d405197a635e1b08590293a31f1aa8670e8879b
farid <jefferson.farid@hotmai.com>
2020-05-02 17:18:56 -0500

    clase 109 http put patch  02/05/2020

--- src/app/contact-detail/edit-contact/edit-contact.component.ts
+++ src/app/contact-detail/edit-contact/edit-contact.component.ts
@@ -5,33 +5,34 @@ import { Contact } from 'src/app/contact.model';
 
 @Component({
   selector: 'app-edit-contact',
-  template: `<app-contact-form 
-              [contact]="contact" 
+  template: `<app-contact-form
+              [contact]="contact"
               (submitContact)="updateContact($event)">
               Update contact
             </app-contact-form>`
 })
 export class EditContactComponent implements OnInit {
-  public contact:Contact;
-  public id:number;
+  public contact: Contact;
+  public id: number;
   constructor(
-    public contactService:ContactsService, 
-    public route:ActivatedRoute,
-    private router:Router,
+    public contactService: ContactsService,
+    public route: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      //use spread operator to separate id from the rest of the contact field
+      // use spread operator to separate id from the rest of the contact field
       const {id, ...contact} = data.contact;
       this.contact = contact;
       this.id = id;
     });
   }
 
-  updateContact(formContact){
-    this.contactService.updateContact({id:this.id, ...formContact});
-    this.router.navigate(['/']);
+  updateContact(formContact) {
+    this.contactService.updateContact({id: this.id, ...formContact}).subscribe(resolved =>  {
+      this.router.navigate(['/']);
+    });
   }
 
 }
--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -9,40 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class ContactsService {
     private contactsSubject = new BehaviorSubject<Contact[]>([]);
 
-    constructor(private http:HttpClient) { 
+    constructor(private http: HttpClient) {
     }
 
-    getContacts(){
+    getContacts() {
         return this.http.get<Contact[]>('http://localhost:3000/contacts');
     }
 
-    getContactById(id){
+    getContactById(id) {
         return this.http.get<Contact>(`http://localhost:3000/contacts/${id}`);
     }
 
-    public addContact(contact:Contact){
+    public addContact(contact: Contact) {
         return this.http.post('http://localhost:3000/contacts', contact);
     }
 
-    public updateContact(contact:Contact){
-        const contacts = this.contactsSubject.value;
-        let replaceIndex = contacts.findIndex( item => item.id == contact.id);
-        const newContacts = [
-            ...contacts.slice(0,replaceIndex), 
-            contact, 
-            ...contacts.slice(replaceIndex+1)
-        ];
-        this.contactsSubject.next(newContacts);
+    public updateContact(contact: Contact) {
+        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, contact);
     }
 
-    public removeContact(contact:Contact){
+    public removeContact(contact: Contact) {
         const contacts = this.contactsSubject.value;
-        let replaceIndex = contacts.findIndex( item => item.id == contact.id);
+        const replaceIndex = contacts.findIndex( item => item.id == contact.id);
         const newContacts = [
-            ...contacts.slice(0,replaceIndex), 
-            ...contacts.slice(replaceIndex+1)
+            ...contacts.slice(0, replaceIndex),
+            ...contacts.slice(replaceIndex + 1)
         ];
-        this.contactsSubject.next(newContacts);                
+        this.contactsSubject.next(newContacts);
     }
 
 }
