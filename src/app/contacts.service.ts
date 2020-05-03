9bf0ae90c770fb0013b54cf84a018bd09cad8a59
farid <jefferson.farid@hotmai.com>
2020-05-03 13:14:18 -0500

    clase 111 url environments   03/05/2020

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -3,43 +3,46 @@ import { Contact, PhoneType} from './contact.model';
 import { of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { mergeMap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ContactsService {
-    constructor(private http:HttpClient) { 
+    private resourceUrl = environment.apiUrl + '/contacts';
+    constructor(private http: HttpClient) {
     }
 
-    getContacts(){
-        return this.http.get<Contact[]>('http://localhost:3000/contacts');
+    getContacts() {
+        return this.http.get<Contact[]>(this.resourceUrl);
     }
 
-    getContactById(id){
-        return this.http.get<Contact>(`http://localhost:3000/contacts/${id}`);
+    getContactById(id) {
+        return this.http.get<Contact>(`${this.resourceUrl}/${id}`);
     }
 
-    public addContact(contact:Contact){
-        return this.http.post('http://localhost:3000/contacts', contact).pipe(
-            mergeMap( (res:Contact) => this.updateContactImage(res, contact.pictureFile) )
+    public addContact(contact: Contact) {
+        return this.http.post(this.resourceUrl, contact).pipe(
+            mergeMap( (res: Contact) => this.updateContactImage(res, contact.pictureFile) )
         );
     }
 
-    public updateContact(contact:Contact){
-        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, contact).pipe( mergeMap( (res:Contact) => this.updateContactImage(res, contact.pictureFile) ));
+    public updateContact(contact: Contact) {
+        return this.http.patch(`${this.resourceUrl}/${contact.id}`, contact).pipe(
+            mergeMap( (res: Contact) => this.updateContactImage(res, contact.pictureFile) ));
     }
 
-    private updateContactImage(contact:Contact, file:File){
-        if(!file){
+    private updateContactImage(contact: Contact, file: File) {
+        if (!file) {
             return of(contact);
         }
-        let formData = new FormData();
+        const formData = new FormData();
         formData.append('picture', file, file.name);
-        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, formData);
+        return this.http.patch(`${this.resourceUrl}/${contact.id}`, formData);
     }
 
-    public removeContact(contact:Contact){
-        return this.http.delete(`http://localhost:3000/contacts/${contact.id}`);
+    public removeContact(contact: Contact) {
+        return this.http.delete(`${this.resourceUrl}/${contact.id}`);
     }
 
 }
