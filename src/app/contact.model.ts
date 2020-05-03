7add1ccff513295eba992334ab8016ab60c67d66
farid <jefferson.farid@hotmai.com>
2020-05-03 12:41:23 -0500

    clase 109 http put img upload  03/05/2020

--- src/app/contact.model.ts
+++ src/app/contact.model.ts
@@ -1,21 +1,21 @@
-export class Contact{
+export class Contact {
     constructor(
-        public id:number,
-        public name:string,
-        public picture:string,
-        public phones:ContactPhoneI[],
-        public email:string,
-        public address:string,
-        public pictureFile?:File
-    ){ }
+        public id: number,
+        public name: string,
+        public picture: string,
+        public phones: ContactPhoneI[],
+        public email: string,
+        public address: string,
+        public pictureFile?: File
+    ) { }
 }
 
-export interface ContactPhoneI{
-    type:PhoneType;
-    number:number;
+export interface ContactPhoneI {
+    type: PhoneType;
+    number: number;
 }
 
-export enum PhoneType{
+export enum PhoneType {
     mobile = 'mobile',
     home = 'home',
     work = 'work'
--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Contact, PhoneType} from './contact.model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -9,33 +10,46 @@ import { HttpClient } from '@angular/common/http';
 export class ContactsService {
     private contactsSubject = new BehaviorSubject<Contact[]>([]);
 
-    constructor(private http:HttpClient) { 
+    constructor(private http: HttpClient) {
     }
 
-    getContacts(){
+    getContacts() {
         return this.http.get<Contact[]>('http://localhost:3000/contacts');
     }
 
-    getContactById(id){
+    getContactById(id) {
         return this.http.get<Contact>(`http://localhost:3000/contacts/${id}`);
     }
 
-    public addContact(contact:Contact){
-        return this.http.post('http://localhost:3000/contacts', contact);
+    public addContact(contact: Contact) {
+        return this.http.post('http://localhost:3000/contacts', contact).pipe(
+            mergeMap((res: Contact) => this.updateContactImage(res, contact.pictureFile))
+        );
     }
 
-    public updateContact(contact:Contact){
-        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, contact);
+    public updateContact(contact: Contact) {
+        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, contact).pipe(
+            mergeMap((res: Contact) => this.updateContactImage(res, contact.pictureFile))
+        );
     }
 
-    public removeContact(contact:Contact){
+    private updateContactImage(contact: Contact, file: File) {
+        if (!file) {
+            return of(contact);
+        }
+        const formData = new FormData();
+        formData.append('picture', file, file.name);
+        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, formData);
+    }
+
+    public removeContact(contact: Contact) {
         const contacts = this.contactsSubject.value;
-        let replaceIndex = contacts.findIndex( item => item.id == contact.id);
+        const replaceIndex = contacts.findIndex( item => item.id == contact.id);
         const newContacts = [
-            ...contacts.slice(0,replaceIndex), 
-            ...contacts.slice(replaceIndex+1)
+            ...contacts.slice(0, replaceIndex),
+            ...contacts.slice(replaceIndex + 1)
         ];
-        this.contactsSubject.next(newContacts);                
+        this.contactsSubject.next(newContacts);
     }
 
 }
